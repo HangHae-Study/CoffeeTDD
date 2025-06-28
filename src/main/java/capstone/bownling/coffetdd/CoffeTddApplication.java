aded0f9dc1269cb2d90fb900c057bb25df30f76c
SUNGIN99 <99_insung@naver.com>
2025-06-28 10:19:27 +0900

    First Commit

---
+++ src/main/java/capstone/bownling/coffetdd/CoffeTddApplication.java
@@ -0,0 +1,13 @@
+package capstone.bownling.coffetdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/capstone/bownling/coffetdd/CoffeTddApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.bownling.coffetdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
